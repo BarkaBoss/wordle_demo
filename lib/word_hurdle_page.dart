b5b855d96b9da364882f2aa50d9c70eae75bd2f9
Barka Tatama <bossmajor.biii@gmail.com>
2024-05-12 13:52:12 +0100

    Added Wordle Grid

--- lib/word_hurdle_page.dart
+++ lib/word_hurdle_page.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wordle/hurlde_provider.dart';
+import 'package:wordle/wordle_view.dart';
 
 class WordHurdle extends StatefulWidget {
   const WordHurdle({super.key});
@@ -26,18 +28,21 @@ class _WordHurdleState extends State<WordHurdle> {
       body: Center(
         child: Column(
           children: [
-            Consumer<HurdleProvider>(
-              builder: (context, provider, child) => GridView.builder(
-                  gridDelegate: const  SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 5,
-                    mainAxisSpacing: 4,
-                    crossAxisSpacing: 4,
-                  ),
-                  itemCount: provider.hurdleBoard.length,
-                  itemBuilder: (context, index){
-                    provider.hurdleBoard[index];
-                  }
-                  ),
+            Expanded(
+              child: Consumer<HurdleProvider>(
+                builder: (context, provider, child) => GridView.builder(
+                    gridDelegate: const  SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 5,
+                      mainAxisSpacing: 4,
+                      crossAxisSpacing: 4,
+                    ),
+                    itemCount: provider.hurdleBoard.length,
+                    itemBuilder: (context, index){
+                      final wordle = provider.hurdleBoard[index];
+                      return WordleView(wordle: wordle); 
+                    }
+                    ),
+              ),
             )
           ],
         ),
